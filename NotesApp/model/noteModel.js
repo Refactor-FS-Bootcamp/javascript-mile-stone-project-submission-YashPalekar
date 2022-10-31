c0ea7ef42d57cbd7f40d2fd2972a224e610f1c78
Yash Palekar <49448619+YashPalekar@users.noreply.github.com>
2022-10-31 22:18:27 +0530

    Delete NotesApp directory

--- NotesApp/model/noteModel.js
+++
@@ -1,9 +0,0 @@
-export default class Note {
-    constructor() {
-        this.id = 0;
-        this.title = '';
-        this.description = '';
-        this.color = '#444';
-        this.date = new Date().toLocaleDateString();
-    }
-}
